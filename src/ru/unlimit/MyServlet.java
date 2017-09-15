298e4c79dfd280ade52e901f24649a9005ad05a6
Maksim Kniha <maksimbeta777@mail.ru>
2017-09-15 17:24:56 +0300

    Project8

--- src/ru/unlimit/MyServlet.java
+++ src/ru/unlimit/MyServlet.java
@@ -2,6 +2,7 @@ package ru.unlimit;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,11 +16,51 @@ public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Info");
+		
+		String name = request.getParameter("firstName");
+		String surname = request.getParameter("secondName");
+//		String job = request.getParameter("job");
+		String jobs[] = request.getParameterValues("job");
+		String gender = request.getParameter("gender");
+		if(gender==null){
+			gender = "-";
+		}
+		String age18 = request.getParameter("age18");
+		if(age18==null){
+			age18 = "-";
+		}
+		System.out.println(name);
+		System.out.println(surname);
+		System.out.println(Arrays.toString(jobs));
+		System.out.println(gender);
+		System.out.println(age18);
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
-		out.println("<h3>Hello from Максим</h3><br>Again");
+		out.println("<h3>Профиль сотрудника:</h3>");
+		out.println("Имя: "+name+"<br>");
+		out.println("Фамилия: "+surname+"<br>");
+//		out.println("Профессия: "+job+"<br>");
+		out.println("Профессии: "+jobs.length+" ");
+		out.println(Arrays.toString(jobs)+"<br>");
+//		for (String job : jobs){
+//			out.println(job+"<br>");
+//		}
+		out.println("Пол: "+gender+"<br>");
+		out.println("Старше 18: "+age18+"<br>");
+		out.close();
+	}
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("firstName");
+		String surname = request.getParameter("secondName");
+		System.out.println(name);
+		System.out.println(surname);
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello from Servlet.Post "+name+" "+surname+"</h3>");
 		out.close();
 	}
 
